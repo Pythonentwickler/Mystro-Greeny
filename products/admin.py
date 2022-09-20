d4c508576118252056d7acbf5c6eb7aad2b5ca92
Mohamad Juma <pythonentwickler76@gmail.com>
2022-09-20 23:22:51 +0200

    design product models & add to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product,ProductImage,ProductReview,Category,Brand
+
+class ProductImagAdmin(admin.TabularInline,):
+    model = ProductImage
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'flag', 'price']
+    inlines = [ProductImagAdmin]
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductReview)
+admin.site.register(ProductImage)
+admin.site.register(Category)
+admin.site.register(Brand)
